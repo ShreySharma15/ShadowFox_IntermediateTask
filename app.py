cb3f8087ce0ac17876aea89cd7896743999d36d8
ShreySharma15 <shrey1atharv@gmail.com>
2025-10-30 00:07:39 +0530

    Files updated

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# Save this as app.py after downloading your .pkl files to a folder
-
-from flask import Flask, request, render_template
+import streamlit as st
 import pickle
 import numpy as np
 
@@ -10,29 +8,22 @@ le_fuel = pickle.load(open('le_fuel_type.pkl', 'rb'))
 le_seller = pickle.load(open('le_seller_type.pkl', 'rb'))
 le_trans = pickle.load(open('le_transmission.pkl', 'rb'))
 
-app = Flask("CarValueAI")
+st.title("Welcome to CarValueAI")
 
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        # Get data from form
-        present_price = float(request.form['present_price'])
-        kms_driven = int(request.form['kms_driven'])
-        fuel_type = request.form['fuel_type']
-        seller_type = request.form['seller_type']
-        transmission = request.form['transmission']
-        owner = int(request.form['owner'])
-        car_age = int(request.form['car_age'])
-        
-        # Encode string inputs
-        fuel_type_enc = le_fuel.transform([fuel_type])[0]
-        seller_type_enc = le_seller.transform([seller_type])[0]
-        trans_enc = le_trans.transform([transmission])[0]
-        
-        features = np.array([[present_price, kms_driven, fuel_type_enc, seller_type_enc, trans_enc, owner, car_age]])
-        prediction = model.predict(features)[0]
-        return render_template('index.html', prediction=round(prediction, 2))
-    return render_template('index.html', prediction=..., appname="CarValueAI")
+present_price = st.number_input("Present Price (Lakhs)", min_value=0.01, step=0.01)
+kms_driven = st.number_input("Kms Driven", min_value=0)
+fuel_type = st.selectbox("Fuel Type", ('Petrol', 'Diesel', 'CNG'))
+seller_type = st.selectbox("Seller Type", ('Dealer', 'Individual'))
+transmission = st.selectbox("Transmission", ('Manual', 'Automatic'))
+owner = st.number_input("Owner", min_value=0, max_value=3, step=1)
+car_age = st.number_input("Car Age (years)", min_value=0, step=1)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Predict"):
+    # Encode inputs
+    fuel_enc = le_fuel.transform([fuel_type])[0]
+    seller_enc = le_seller.transform([seller_type])[0]
+    trans_enc = le_trans.transform([transmission])[0]
+
+    features = np.array([[present_price, kms_driven, fuel_enc, seller_enc, trans_enc, owner, car_age]])
+    prediction = model.predict(features)[0]
+    st.success(f"Predicted Selling Price: ₹{prediction:.2f} Lakhs")
